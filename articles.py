cfc811768be948c5c4cb178d3fa637235d1cf4ff
Richard Wattenbarger <richard@wattenbarger.net>
2024-11-26 15:01:46 -0500

    center the main window on startup

--- articles.py
+++ articles.py
@@ -31,6 +31,8 @@ answers = 'der die das die den die das die dem der dem den des der des der'.spli
 
 class App(tk.Tk):
 
+    # region constants 
+
     _SHOW_ANSWERS_CAPTION = 'Show answers'
     _HIDE_ANSWERS_CAPTION = 'Hide answers'
 
@@ -38,30 +40,33 @@ class App(tk.Tk):
     _FONT_SIZE = 14
     _DEFAULT_FONT = (_FONT_NAME, _FONT_SIZE, 'normal')
     _BOLD_FONT = (_FONT_NAME, _FONT_SIZE, 'bold')
+    
+    # endregion
 
     def __init__(self):
         super().__init__()
         self.title(APP_TITLE)
+        self.bind('<Visibility>', self.center)
 
         GENDERS = ['Masculine', 'Feminine', 'Neuter', 'Plural']
         CASES = ['Nominative', 'Accusative', 'Dative', 'Genitive']
 
-        main_content_frame = tk.Frame(self, padx=5, pady=5)
-        main_content_frame.pack()
+        self.main_content_frame = tk.Frame(self, padx=5, pady=5)
+        self.main_content_frame.pack()
 
         for i, case_ in enumerate(CASES, start=1):
-            tk.Label(main_content_frame, 
+            tk.Label(self.main_content_frame, 
                      text=case_,
                      font=App._BOLD_FONT).grid(row=0, column=i)
 
         for i, gender in enumerate(GENDERS, start=1):
-            tk.Label(main_content_frame, 
+            tk.Label(self.main_content_frame, 
                      text=gender,
                      font=App._BOLD_FONT).grid(row=i, column=0, sticky=tk.W)
 
         self.answer_variables = [tk.StringVar(self) for _ in range(16)]
 
-        self.answer_entries = [tk.Entry(main_content_frame,
+        self.answer_entries = [tk.Entry(self.main_content_frame,
                                    textvariable = self.answer_variables[i],
                                    relief=tk.SUNKEN,
                                    borderwidth=3,
@@ -71,28 +76,30 @@ class App(tk.Tk):
                               sticky=None)
         
         for i in range(0,5):
-            main_content_frame.columnconfigure(i, weight=1)
+            self.main_content_frame.columnconfigure(i, uniform="entries")
 
-        button_frame = tk.Frame(self)
-        button_frame.pack(pady=5)
+        self.button_frame = tk.Frame(self)
+        self.button_frame.pack(pady=5)
         
         self.show_hide_answers_button_caption = tk.StringVar(self)
         self.show_hide_answers_button_caption.set(App._SHOW_ANSWERS_CAPTION)
         self.show_hide_answers_button = \
-                tk.Button(button_frame,
+                tk.Button(self.button_frame,
                           textvariable=self.show_hide_answers_button_caption,
                           command=self._show_hide_answers)
         self.show_hide_answers_button.pack(side='left')
 
-        self.check_answers_button = tk.Button(button_frame,
+        self.check_answers_button = tk.Button(self.button_frame,
                                               text='Check answers',
                                               command=self._check_answers)
         self.check_answers_button.pack(side='left')
 
-        self.clear_answers_button = tk.Button(button_frame,
+        self.clear_answers_button = tk.Button(self.button_frame,
                                               text='Clear answers',
                                               command=self._clear_answers)
         self.clear_answers_button.pack(side='left')
+
+        self._set_tab_order()
 
     def _show_hide_answers(self):
         if self.show_hide_answers_button_caption.get() == App._SHOW_ANSWERS_CAPTION:
@@ -118,7 +125,20 @@ class App(tk.Tk):
         for answer_entry in self.answer_entries:
             answer_entry.configure(fg='black', font=App._DEFAULT_FONT)
 
+    def _set_tab_order(self):
+        for i in range(len(self.answer_entries)):
+            self.answer_entries[(i // 4) + (i % 4) * 4].lift() 
 
+    def center(self, event) -> None:
+        
+        window_height = self.winfo_height()
+        window_width = self.winfo_width()
+        screen_height = self.winfo_screenheight()
+        screen_width = self.winfo_screenwidth()
+        top = (screen_height - window_height) // 2
+        left = (screen_width - window_width) // 2
+        
+        self.geometry(f'{window_width}x{window_height}+{left}+{top}')
 
 def main():
     App().mainloop()
